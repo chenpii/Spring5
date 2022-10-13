624c5857febbb884d6f8156ef5b15b56b7ecd266
chenp <641760512@qq.com>
2022-10-13 09:58:35 +0800

    AOP（基于注解）测试代码

--- spring5_demo4/src/test/java/com/chenp/spring5/AopTest.java
+++ spring5_demo4/src/test/java/com/chenp/spring5/AopTest.java
@@ -1,9 +1,11 @@
 package com.chenp.spring5;
 
 import com.chenp.spring5.aopanno.User;
+import com.chenp.spring5.aopxml.Book;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author chenpi
@@ -17,5 +19,14 @@ public class AopTest {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.chenp.spring5.config");
         User user = context.getBean("user", User.class);
         user.add();
+    }
+
+    @Test
+    public void testAopXml() {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        Book book = context.getBean("book", Book.class);
+        book.buy();
+
     }
 }
